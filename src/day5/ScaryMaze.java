814db977bcde2c51dd6c79c7ebb67bed098051cd
Mrfuzzy <league@iMac.attlocal.net>
2015-11-11 18:28:07 -0800

    Day 5
    We made a maze that was scary.

--- src/day5/ScaryMaze.java
+++ src/day5/ScaryMaze.java
@@ -16,20 +16,21 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
-	
+
 	BufferedImage maze;
-	final int frameWidth = 600;
-	final int frameHeight = 400;
+	final int frameWidth = 800;
+	final int frameHeight = 600;
 
 	ScaryMaze() throws Exception {
-		//1. Use this online tool to make a maze image and drop it into your day5 package: http://pixlr.com/editor/
-		maze = ImageIO.read(getClass().getResource("maze.png"));
-		//2. set the mouse pointer to the start of your maze using:
-		//new Robot().mouseMove(xPosition, yPosition)
-		
-		//3. add a mouse motion listener using:
-		//addMouseMotionListener(this)
-		
+		// 1. Use this online tool to make a maze image and drop it into your
+		// day5 package: http://pixlr.com/editor/
+		maze = ImageIO.read(getClass().getResource("Scary Maze.jpg"));
+		// 2. set the mouse pointer to the start of your maze using:
+		// new Robot().mouseMove(xPosition, yPosition)
+		new Robot().mouseMove(50, 90);
+		// 3. add a mouse motion listener using:
+		// addMouseMotionListener(this)
+		addMouseMotionListener(this);
 	}
 
 	@Override
@@ -37,29 +38,38 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		int mouseX = e.getX();
 		int mouseY = e.getY();
 		int mouseColor = maze.getRGB(mouseX, mouseY);
-		//4. print the mouseColor variable to see what color the mouse is touching
-
-		//5. make a variable to hold the background color. 
-
-		//6. if the mouse falls off the path (if it is on the background)
-		
-				// call the scare method
-		
-		//10. if the mouse is on the end color
-				
-				// pop up a message to tell them they won
-		
+		// 4. print the mouseColor variable to see what color the mouse is
+		// touching
+		System.out.println(mouseColor);
+		// 5. make a variable to hold the background color.
+		int backgroundcolor = -1;
+		// 6. if the mouse falls off the path (if it is on the background)
+		if (mouseColor == -1) {
+			// call the scare method
+			scare();
+		}
+		// 10. if the mouse is on the end color
+		else if (mouseColor == -65528) {
+			// pop up a message to tell them they won
+			System.out.println("You Win");
+			AudioClip clap = JApplet.newAudioClip(getClass().getResource("Win.wav"));
+			clap.play();
+		}
 	}
 
 	private void scare() {
 		System.out.println("BOO!");
-		//7. find a scary sound and put it in the day5 package where you put your maze picture. You can find a sound on freesound.org. Log in as leagueofamazing/code4life.
-		//AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav"));
-		
-		//8. play the scary sound. Hint: type "sound" and then a period.		
-		
-		//9. drop an image into your day5 package, and use the showScaryImage method to scare your victim!
-
+		// 7. find a scary sound and put it in the day5 package where you put
+		// your maze picture. You can find a sound on freesound.org. Log in as
+		// leagueofamazing/code4life.
+		// AudioClip sound =
+		// JApplet.newAudioClip(getClass().getResource("scream.wav"));
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource("ScarySound.wav"));
+		// 8. play the scary sound. Hint: type "sound" and then a period.
+		sound.play();
+		// 9. drop an image into your day5 package, and use the showScaryImage
+		// method to scare your victim!
+		showScaryImage("Ghost.png");
 	}
 
 	private void showScaryImage(String imageName) {
@@ -77,7 +87,7 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 
 	@Override
 	public void run() {
-		JFrame frame = new JFrame("June's Scary Maze");
+		JFrame frame = new JFrame("Maze");
 		frame.add(this);
 		setPreferredSize(new Dimension(frameWidth, frameHeight));
 		frame.pack();
@@ -92,9 +102,7 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 	}
 
 	@Override
-	public void mouseDragged(MouseEvent e) {}
+	public void mouseDragged(MouseEvent e) {
+	}
 
 }
-
-
-
